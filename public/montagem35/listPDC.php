e4f98dbf565b77714d803bac5a4fed03494e5639
Vinicius <vinicius.sm6@gmail.com>
2021-06-15 10:15:03 -0300

    User/CSS

--- public/montagem35/listPDC.php
+++ public/montagem35/listPDC.php
@@ -1,5 +1,3 @@
-
-
 <?php include("../Template/header.php") ?>
 
 <?php include("../../data/connection.php") ?>
@@ -9,30 +7,30 @@
 <br>
 
 <body>
-    
-    <div style="margin-left: 100px; margin-right: 100px;">
-        <h2>Planilhas de controle</h2>
 
-        <br>
-        
-    <div class="buttons">
-                <a href="../montagem35/createPDC.php" class="btn btn-primary">Cadastre uma planilha de controle</a>
-        </div>
-    
-    <br>
-    <form method="POST" id="form-pesquisa" action="">
-	<div class="input-group mb-3">
-	<div class="input-group-prepend">
-                    <span class="input-group-text" id="inputGroup-sizing-default">Pesquisar planilhas de controle</span>
-                </div>
+	<div style="margin-left: 100px; margin-right: 100px;">
+		<h2>Planilhas de controle</h2>
+
+		<br>
+
+		<div class="buttons">
+			<a href="../montagem35/createPDC.php" class="btn btn-primary">Cadastre uma planilha de controle</a>
+		</div>
+
+		<br>
+		<form method="POST" id="form-pesquisa" action="">
+			<div class="input-group mb-3">
+				<div class="input-group-prepend">
+					<span class="input-group-text" id="inputGroup-sizing-default">Pesquisar planilhas de controle</span>
+				</div>
 				<input type="text" name="pesquisa" class="form-control" id="pesquisa" aria-label="Default" placeholder="Digite o número da etiqueta" aria-describedby="inputGroup-sizing-default">
+			</div>
 	</div>
-    </div>
-		
-    </form>
-    <ul class="resultado">
 
-	<?php include("listPDCNoSearch.php") ?>
+	</form>
+	<ul class="resultado">
+
+		<?php include("listPDCNoSearch.php") ?>
 
 	</ul>
 </body>
